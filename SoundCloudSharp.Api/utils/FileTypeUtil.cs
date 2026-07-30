a725b79a91ebd3c23e31f1c15dc6aefb86b9ba76
Fleacon <david.noskov0603@gmail.com>
2026-07-31 01:06:18 +0200

    Update supported audio types
    based on:
    https://help.soundcloud.com/hc/en-us/articles/360039171614-Upload-Requirements#:~:text=Supported%20audio%20file%20formats

--- SoundCloudSharp.Api/utils/FileTypeUtil.cs
+++ SoundCloudSharp.Api/utils/FileTypeUtil.cs
@@ -2,13 +2,22 @@ namespace SoundCloudSharp.Api.utils;
 
 public static class FileTypeUtil
 {
+    // https://help.soundcloud.com/hc/en-us/articles/360039171614-Upload-Requirements#:~:text=Supported%20audio%20file%20formats
     public static string GetAudioContentType(string filename) => Path.GetExtension(filename).ToLowerInvariant() switch
     {
-        ".mp3" => "audio/mpeg",
+        ".mp3" or ".mp2" => "audio/mpeg",
         ".wav" => "audio/wav",
         ".flac" => "audio/flac",
         ".ogg" => "audio/ogg",
-        _ => "application/octet-stream"
+        ".aiff" => "audio/aiff",
+        ".aac" => "audio/aac",
+        ".mp4" or ".m4a" => "audio/mp4",
+        ".3gp" => "video/3gp",
+        ".3g2" => "audio/3gpp2",
+        ".mj2" => "video/mj2",
+        ".wma" => "audio/x-ms-wma",
+        ".amr" => "audio/amr",
+        _ => throw new Exception("Unknown audio format.")
     };
 
     public static string GetImageContentType(string filename) => Path.GetExtension(filename).ToLowerInvariant() switch
