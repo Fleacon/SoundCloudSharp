0841875ae52c24e49ab60264612ae22e92573bc0
Fleacon <david.noskov0603@gmail.com>
2026-08-16 20:04:04 +0200

    Fix to Utc time

--- SoundCloudSharp.Api/Models/Auth/OAuthToken.cs
+++ SoundCloudSharp.Api/Models/Auth/OAuthToken.cs
@@ -5,9 +5,9 @@ public record OAuthToken
     public string AccessToken { get; init; }
     public string RefreshToken { get; init; }
     public int ExpiresIn { get; init; }
-    public string Scope { get; init; }
-    public AuthType Auth { get; init; }
+    public string? Scope { get; init; }
+    public AuthType? Auth { get; init; }
     
-    public DateTime CreatedAt { get; init; } = DateTime.Now;
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
     public bool IsExpired => CreatedAt.AddSeconds(ExpiresIn) <= DateTime.UtcNow;
 }
