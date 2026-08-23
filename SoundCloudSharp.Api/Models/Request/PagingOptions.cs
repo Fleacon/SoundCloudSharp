2f7691afba6c467ea22fa3630f085f257beacd21
Fleacon <david.noskov0603@gmail.com>
2026-08-23 18:56:18 +0200

    Set linked partitioning to be always true
    Because the library doesn't support deprecated endpoints and lists without pagination is considered deprecated: removed the option to receive lists without pagination

--- SoundCloudSharp.Api/Models/Request/PagingOptions.cs
+++ SoundCloudSharp.Api/Models/Request/PagingOptions.cs
@@ -7,5 +7,5 @@ public record PagingOptions : IQueryParameterContainer
     [QueryParam("limit")]
     public int? Limit { get; init; }
     [QueryParam("linked_partitioning")]
-    public bool LinkedPartitioning { get; init; } = true;
+    public bool LinkedPartitioning => true;
 }
